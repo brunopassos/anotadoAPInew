c89f72b293f65b48ecfba1dcd1b565ee74b172c9
Bruno Passos <brunopassos_bp@hotmail.com>
2023-04-23 11:59:24 -0300

    feat: implemented middleware to ferify if user ir note owner

--- src/routers/notes.routes.ts
+++ src/routers/notes.routes.ts
@@ -3,12 +3,13 @@ import ensureNoteDataIsValidMiddleware from "../middlewares/ensureNoteDataIsVali
 import { noteSchema } from "../schemas/note.schemas";
 import {createNoteController, deleteNoteController, listNotesController, updateNoteController} from "../controllers/notes.controllers";
 import ensureTokenIsValidMiddleware from "../middlewares/ensureTokenIsValid.middleware";
+import ensureUserIsNoteOwner from "../middlewares/ensureUserIsNoteOwner.middleware";
 
 const noteRoutes: Router = Router();
 
 noteRoutes.post("", ensureTokenIsValidMiddleware, ensureNoteDataIsValidMiddleware(noteSchema), createNoteController);
 noteRoutes.get("", listNotesController);
-noteRoutes.patch("/:id", ensureTokenIsValidMiddleware, updateNoteController);
-noteRoutes.delete("/:id", ensureTokenIsValidMiddleware, deleteNoteController);
+noteRoutes.patch("/:id", ensureTokenIsValidMiddleware, ensureUserIsNoteOwner,updateNoteController);
+noteRoutes.delete("/:id", ensureTokenIsValidMiddleware, ensureUserIsNoteOwner, deleteNoteController);
 
 export default noteRoutes;
